55f0d57910f46ca70e818006fa49b5093c2b0a0b
Guillem <guillem.aldeguer@gmail.com>
2014-11-06 21:53:21 +0100

    Modify MixerException

--- Source/Core/Exception/cGenericException.hpp
+++ Source/Core/Exception/cGenericException.hpp
@@ -6,7 +6,9 @@
 #include <string>
 
 #include "SDL/SDL.h"
+#include "SDL/SDL_mixer.h"
 #include "SDL/SDL_ttf.h"
+#include "SDL/SDL_image.h"
 
 class GenericException : public std::exception {
 	public:
